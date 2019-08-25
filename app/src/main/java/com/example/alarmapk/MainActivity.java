e10905f05c6e37e05f058a8b5bba05297c72d266
Benjamin <bdzanko1@gmail.com>
2019-08-25 16:50:41 +0200

    testic jos jedan

--- app/src/main/java/com/example/alarmapk/MainActivity.java
+++ app/src/main/java/com/example/alarmapk/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("master branch");
+        System.out.println("master branchh");
     }
 }
