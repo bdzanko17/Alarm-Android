3c4e50fcd3707e979e78340396adde8f255224e6
adnan4894 <adnanbuljubasic1@gmail.com>
2019-08-26 20:51:51 +0200

    Two new activities(Settings,addAlarm).
    Implemented onClick method(transition from the main activity to settings acitivity).
    
    Implemented onClick method(transition from the main activity to Alarm acitivity);

--- app/src/main/java/com/example/alarmapk/MainActivity.java
+++ app/src/main/java/com/example/alarmapk/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.alarmapk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button settingButton,addAlarm;
+
+    //onClick method from main to setting acitivity//
+    public void settingActivity(View view){
+        Intent intent = new Intent(MainActivity.this, Settings.class);
+        startActivity(intent);
+    }
+    //onClick method from main to addAlarm activity//
+    public void addAlarmActivity(View view){
+        Intent intent = new Intent(MainActivity.this, Alarm.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("First pull request");
 
-        //BELJO
+        settingButton=findViewById(R.id.settingsButton);
+        addAlarm=findViewById(R.id.settingsButton);
+
+
     }
 }
