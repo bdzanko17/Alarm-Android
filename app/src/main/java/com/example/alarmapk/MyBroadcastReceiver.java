bb257a2e7e1f6721b3ffab71da3d16c5a19ca5d8
Benjamin <bdzanko1@gmail.com>
2019-08-27 21:05:39 +0200

    Added new things, notification when alarm start ringing, timePikcerFragment, Receiver for alarm, also added some stupid xml in alarm_activity

---
+++ app/src/main/java/com/example/alarmapk/MyBroadcastReceiver.java
@@ -0,0 +1,39 @@
+package com.example.alarmapk;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build()); //za notifikaciju kad pocne alarm
+
+        Toast.makeText(context, "ALARMMM", Toast.LENGTH_LONG).show();
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000); //vibracija
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        Ringtone r = RingtoneManager.getRingtone(context, sound);
+        r.play(); //zvuk m
+
+        //Slijedeci koraci su kkako ugasiti alarm
+
+
+
+    }
+
+}
+
+
