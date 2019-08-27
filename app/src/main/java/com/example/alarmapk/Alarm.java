7f6302e087f2178f5ef743125aa6631ad45eb021
adnan4894 <adnanbuljubasic1@gmail.com>
2019-08-27 17:34:28 +0200

    AddAlarm_acitivity UI.

--- app/src/main/java/com/example/alarmapk/Alarm.java
+++ app/src/main/java/com/example/alarmapk/Alarm.java
@@ -1,13 +1,54 @@
 package com.example.alarmapk;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class Alarm extends AppCompatActivity {
+
+    TimePicker picker;
+    Button btnGet;
+    TextView tvw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        tvw=findViewById(R.id.textView1);
+        picker=findViewById(R.id.timePicker1);
+        picker.setIs24HourView(true);
+        btnGet=findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour, minute;
+                String am_pm;
+                if (Build.VERSION.SDK_INT >= 23 ){
+                    hour = picker.getHour();
+                    minute = picker.getMinute();
+                }
+                else{
+                    hour = picker.getCurrentHour();
+                    minute = picker.getCurrentMinute();
+                }
+                if(hour > 12) {
+                    am_pm = "PM";
+                    hour = hour - 12;
+                }
+                else
+                {
+                    am_pm="AM";
+                }
+                tvw.setText("Selected Date: "+ hour +":"+ minute+" "+am_pm);
+
+            }
+        });
+
     }
 }
