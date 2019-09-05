07448dce343d47fe764c7725b2b9726a7fc9e728
adnan4894 <adnanbuljubasic1@gmail.com>
2019-09-05 16:01:45 +0200

    Notification Alarm(Turn OFF)

---
+++ app/src/main/java/com/example/alarmapk/MyBroadcastReceiverOff.java
@@ -0,0 +1,26 @@
+package com.example.alarmapk;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.os.Vibrator;
+
+import androidx.core.app.NotificationCompat;
+
+
+
+public class MyBroadcastReceiverOff extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        MyBroadcastReceiver.r.stop();
+
+
+    }
+}
